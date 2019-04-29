f06f01768926d08b855c5bb1dbe51cd2ea287367
Victoria Repo <a1688194@student.adelaide.edu.au>
2019-04-29 21:32:44 +0930

    added features to bst

--- bst.cpp
+++ bst.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 typedef struct Node
 {
-	int value;
+	int data;
 	Node *left;
 	Node *right;
 } Node;
@@ -17,15 +17,15 @@ Node* insertNode(Node* node, int val)
 	if (node == NULL)
 	{
 		Node *temp = (Node *)malloc(sizeof(Node));
-		temp->value = val;
+		temp->data = val;
 		temp->left = NULL;
 		temp->right = NULL;
 		return temp;
 	}
 
-	if (val < node->value)
+	if (val < node->data)
 		node->left  = insertNode(node->left, val);
-	else if (val > node->value)
+	else if (val > node->data)
 		node->right = insertNode(node->right, val);
 
 	return node;
@@ -37,9 +37,27 @@ void inorder(Node *root)
 	if (root != NULL)
 	{   
 		inorder(root->left);
-		cout << root->value << " ";
+		cout << root->data << " ";
 		inorder(root->right);
 	}
+}
+
+void postOrder(Node *root) {
+	if (root == NULL)
+		return;
+
+	postOrder(root->left);
+	postOrder(root->right);
+	cout << root->data << " ";
+}
+
+void preOrder(Node *root) {
+	if (root == NULL)
+		return;
+
+	cout << root->data << " ";
+	preOrder(root->left);
+	preOrder(root->right);
 }
 
 // function to print bst in level order
@@ -49,7 +67,7 @@ bool levelOrder(Node *root, int level)
 		return false;
 	else if (level == 0 && root != NULL)
 	{
-		cout << root->value << " ";
+		cout << root->data << " ";
 		return true;
 	}
 	else
@@ -59,13 +77,56 @@ bool levelOrder(Node *root, int level)
 
 		if (bool1 || bool2)
 			return true;
+		else
+			return false;
+	}
+}
+
+void leftTopView(Node * root)
+{
+	if (root != NULL) {
+		leftTopView(root->left);
+		cout << root->data << " ";
+	}
+}
+
+void topView(Node * root) {
+	leftTopView(root);
+
+	root = root->right;
+
+	while (root != NULL)
+	{
+		cout << root->data << " ";
+		root = root->right;
+	}
+}
+
+// function to find the lowest common ancestor of rwo===two nodes
+int lowestCommonAncestor(Node *root, int node1, int node2)
+{
+	while (true)
+	{
+		if (node1 < root->data && node2 < root->data)
+		{
+			root = root->left;
+		}
+		else if (node1 > root->data && node2 > root->data)
+		{
+			root = root->right;
+		}
+		else
+		{
+			return root->data;
+		}
 	}
 }
 
 int main(int argc, char **argv)
 {
 	// create a binary tree
-	vector<int> numbers = {5,3,2,4,7,6,8};
+	// vector<int> numbers = {5,3,2,4,7,6,8};
+	vector<int> numbers = {1,14,3,7,4,5,15,6,13,10,11,2,12,8,9};
 	Node *root = NULL;
 	root = insertNode(root, numbers.at(0));
 
@@ -73,21 +134,48 @@ int main(int argc, char **argv)
 		insertNode(root, numbers.at(i));
 
 	// print inorder
-	inorder(root);
-	cout << endl;
+	// cout << "Inorder traversal:\n";
+	// inorder(root);
+	// cout << endl;
 
-	int level = 1;
-	int i = 0;
+	// // print preorder
+	// cout << "Preorder traversal:\n";
+	// preOrder(root);
+	// cout << endl;
+
+	// // print postorder
+	// cout << "Postorder traversal:\n";
+	// postOrder(root);
+	// cout << endl;
+
+	// print top view - UNFINISHED
+	// cout << "Top view:\n";
+	// topView(root);
+	// cout << endl;
 
 	// print in level order
-	while (true)
-	{
-		// false is only return when there are no more nodes detected
-		if (!levelOrder(root, i))
-			break;
-		i++;
-	}
-	cout << endl;
+	// int level = 1;
+	// int i = 0;
+	// cout << "Level order traversal:\n";
+	// while (true)
+	// {
+	// 	// false is only return when there are no more nodes detected
+	// 	if (!levelOrder(root, i))
+	// 		break;
+	// 	i++;
+	// 	cout << endl;
+	// }
+	// cout << endl;
+
+	// find lowest common ancestor
+	// int x1 = 2;
+	// int x2 = 6;
+	// cout << "Lowest common ancestor of " << x1 << " and " << x2 << " is: ";
+	// cout << lowestCommonAncestor(root, x1, x2) << endl;
+	// x1 = 2;
+	// x2 = 4;
+	// cout << "Lowest common ancestor of " << x1 << " and " << x2 << " is: ";
+	// cout << lowestCommonAncestor(root, x1, x2) << endl;
 
 	return 0;
 }
